a1689414863836da6073ed670afba81b17131a3f
oliver <caoyingkui@pku.edu.cn>
2017-07-23 22:55:12 +0800

    page

--- src/main/java/cn/edu/pku/sei/SnowView/servlet/CypherQueryServlet.java
+++ src/main/java/cn/edu/pku/sei/SnowView/servlet/CypherQueryServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.File;
+import java.util.List;
 
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import graphsearcher.GraphSearcher;
@@ -20,11 +21,15 @@ import graphsearcher.GraphSearcher;
  */
 public class CypherQueryServlet extends HttpServlet {
 	
-	GraphDatabaseService db=null;
+	GraphDatabaseService db = null;
+	GraphSearcher searcher;
+	List <SearchResult> resultCache;
+	int resultLength;
 	
 	public void init(ServletConfig config) throws ServletException{
-		File databasePath = new File("E:\\SnowGraphData\\dc\\graphdb");
+		File databasePath = new File("I:\\data\\graphdb\\graphdb");
         db = new GraphDatabaseFactory().newEmbeddedDatabase(databasePath);
+        searcher = new GraphSearcher(db);
 	}
 	
     @Override
@@ -38,14 +43,29 @@ public class CypherQueryServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         System.out.println("start query");
         String query = request.getParameter("query");
-        String type = request.getParameter("params");
+        String queryText = request.getParameter("params");
+        String requestType = request.getParameter("type");
        /* type = new String(type.getBytes("GBK") , "GBK");
         System.out.println(type);*/
 
-        GraphSearcher searcher = new GraphSearcher(db);
-        SearchResult results = searcher.querySingle(type);
-        JSONObject result = results.toJSON(db);
-
+        
+        JSONObject searchResult = new JSONObject();
+        int index = 0;
+        if(requestType == null){
+        	resultCache = searcher.query(queryText);
+        	searchResult = resultCache.get(0).toJSON(db);
+	        resultLength = resultCache.size();
+	        index = 0;
+	    }else if(requestType.compareTo("getGraph") == 0){
+	    	index = Integer.parseInt(request.getParameter("index"));
+	    	SearchResult results = resultCache.get(index);
+	    	searchResult = results.toJSON(db);
+	    }
+        
+        JSONObject result = new JSONObject();
+        result.put("searchResult" , searchResult);
+        result.put("index", index);
+        result.put("max" , resultLength);
         System.out.println("end query");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
--- src/main/java/cn/edu/pku/sei/SnowView/servlet/OutGoingRelationServlet.java
+++ src/main/java/cn/edu/pku/sei/SnowView/servlet/OutGoingRelationServlet.java
@@ -23,7 +23,7 @@ public class OutGoingRelationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("id");
 
-        String p = PostUtil.sendGet("http://neo4j:123@127.0.0.1:7474/db/data/node/"+id+"/relationships/all");
+        String p = PostUtil.sendGet("http://neo4j:1@127.0.0.1:7474/db/data/node/"+id+"/relationships/all");
         //System.out.println(p);
         JSONArray jsarr = new JSONArray(p);
         Map<String,Integer> cnt = new HashMap<>();

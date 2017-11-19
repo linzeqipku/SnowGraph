b31aa0f942f333ba199da50f224efb901edbe564
woooking <woooking@qq.com>
2017-11-19 12:53:56 +0800

    change snowview

--- src/main/java/utils/SnowView.java
+++ src/main/java/utils/SnowView.java
@@ -6,7 +6,7 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class SnowView {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
-        WebAppContext ctx = new WebAppContext("webapp", "/SnowGraph");
+        WebAppContext ctx = new WebAppContext("src/main/webapp", "/SnowGraph");
         server.setHandler(ctx);
         server.start();
         server.join();

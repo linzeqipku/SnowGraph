31c9b1deed0463ebc9d0fcda26d381a73c802d13
linzeqi <linzeqi@pku.edu.cn>
2017-11-26 12:57:46 +0800

    simple nav

--- src/main/java/servlet/RandomServlet.java
+++ src/main/java/servlet/RandomServlet.java
@@ -24,30 +24,6 @@ import java.util.Random;
  */
 public class RandomServlet extends HttpServlet {
 
-	private Random rand ;
-	private Map<Long, Pair<Long,Long>> map = new HashMap<>();
-	
-	public void init() throws ServletException{
-
-		rand = new Random();
-
-        /* 读取数据 */
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(Config.getExampleFilePath())),
-                                                                         "UTF-8"));
-            String lineTxt;
-            while ((lineTxt = br.readLine()) != null) {
-            	if (lineTxt.length()==0)
-            		continue;
-                String[] names = lineTxt.split(" ");
-                map.put(Long.parseLong(names[0]), new ImmutablePair<>(Long.parseLong(names[1]), Long.parseLong(names[2])));
-            }
-            br.close();
-        } catch (Exception e) {
-            System.err.println("read errors :" + e);
-        }		
-	}
-	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
@@ -59,12 +35,9 @@ public class RandomServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
-        long id = rand.nextInt(map.size());
-        String query = Config.getDocSearcher().getQuery(map.get(id).getLeft());
+        String query = Config.getRandomExampleQuery();
         JSONObject searchResult = new JSONObject();
         searchResult.put("query", query);
-        searchResult.put("query2", query);
-        searchResult.put("answerId", map.get(id).getRight());
 
         response.getWriter().print(searchResult.toString());
     }
